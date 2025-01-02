b1b7b79269d63c1be248a3be247c7ff084c7fca2
Jaskirat Singh <bedijaskirat2004@gmail.com>
2025-01-02 17:20:00 +0530

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,47 @@
 
 import { ColorModeProvider } from './context/ColorModeContext'; // Import the ColorModeProvider
 import Navbar from './components/layout/Navbar';
-import AppRoutes from './routes';
+// src/routes.jsx
+import { Route, Routes } from 'react-router-dom';
+import AdminLoginPage from './pages/admin/AdminLoginPage';
+import TenantLoginPage from './pages/tenants/TenantLoginPage';
+import TenantRegisterPage from './pages/tenants/TenantRegisterPage';
+import HomePage from './pages/home/HomePage';
+import OtpVerificationPage from './pages/tenants/OtpVerificationPage';
+import AdminDashboard from './pages/admin/AdminDashboardPage';
+import TenantDashboard from './pages/tenants/TenantDashboard';
+import TenantProfilePageForAdmin from './pages/tenants/TenantProfilePageForAdmin';
+import NewPaymentPage from './pages/paymentPage/NewPaymentPage';
 
 const App = () => {
   return (
     <ColorModeProvider>  {/* Wrap the whole app in ColorModeProvider */}
       <>
         <Navbar />
-        <div style={{ display: 'flex' }}>
-          <main style={{ flexGrow: 1, padding: '20px' }}>
-            <AppRoutes />
-          </main>
-        </div>
+        {/* <div style={{ display: 'flex' }}>
+          <main style={{ flexGrow: 1, padding: '20px' }}> */}
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/admin/login" element={<AdminLoginPage />} />
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          <Route path="/admin/tenant/:tenantId" element={<TenantProfilePageForAdmin />} />
+
+          <Route path="/tenant/register" element={<TenantRegisterPage />} />
+          <Route path="/tenant/login" element={<TenantLoginPage />} />
+          <Route path="/tenant/otp-verification" element={<OtpVerificationPage />} />
+
+          {/* <ChakraProvider > */}
+          <Route path="/tenant/dashboard/:tenantId" element={<TenantDashboard />} />
+
+          <Route path="/new-payment" element={<NewPaymentPage />} />
+
+          {/* </ChakraProvider> */}
+
+          {/* <ProtectedRoute path="/admin/dashboard" component={AdminDashboard}/> */}
+        </Routes>
+        {/* </main>
+        </div> */}
       </>
     </ColorModeProvider>
   );
