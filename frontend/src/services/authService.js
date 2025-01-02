a7980630a8d9db949fb57633d42bae5e1175762b
Jaskirat Singh <bedijaskirat2004@gmail.com>
2025-01-02 16:31:59 +0530

    adjusted backend url's in frontend

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // Replace with actual backend API URLs
-// const BASE_URL = `https://pg-management-system.onrender.com/api`;
-const BASE_URL = `http://localhost:5000/api`;
+const BASE_URL = `https://pg-management-system.onrender.com/api`;
+// const BASE_URL = `http://localhost:5000/api`;
 
 export const loginAdmin = async (adminData) => {
   return await axios.post(`${BASE_URL}/admin/login`, adminData);
@@ -13,23 +13,23 @@ export const loginTenant = async (tenantData) => {
 };
 
 export const registerTenant = async (formData) => {
-  const response = await axios.post(`${BASE_URL}/api/tenants`, formData);
+  const response = await axios.post(`${BASE_URL}/tenants`, formData);
   return response.data;
 };
 
 export const sendOtp = async (email) => {
-  const response = await axios.post(`${BASE_URL}/api/tenants/send-otp`, email);
+  const response = await axios.post(`${BASE_URL}/tenants/send-otp`, email);
   return response.data;
 };
 
 export const verifyOtp = async (data) => {
-  const response = await axios.post(`${BASE_URL}/api/tenants/verify-otp`, data);
+  const response = await axios.post(`${BASE_URL}/tenants/verify-otp`, data);
   console.log(response);
 
   return response.data;
 };
 
 export const resendOtp = async (data) => {
-  const response = await axios.post(`${BASE_URL}/api/tenants/resend-otp`, data);
+  const response = await axios.post(`${BASE_URL}/tenants/resend-otp`, data);
   return response.data;
 };
